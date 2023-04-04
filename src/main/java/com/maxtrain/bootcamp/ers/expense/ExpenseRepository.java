fc32f2d48acfa31a013eaa4f5ec87a1e163c25f3
max-s <max-s@DESKTOP-SGG2DJD>
2023-04-04 14:48:19 -0400

    added expense methods

--- src/main/java/com/maxtrain/bootcamp/ers/expense/ExpenseRepository.java
+++ src/main/java/com/maxtrain/bootcamp/ers/expense/ExpenseRepository.java
@@ -1,7 +1,9 @@
 package com.maxtrain.bootcamp.ers.expense;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExpenseRepository extends CrudRepository <Expense, Integer> {
-
+    Optional<Expense> findByStatus(String status);
 }
