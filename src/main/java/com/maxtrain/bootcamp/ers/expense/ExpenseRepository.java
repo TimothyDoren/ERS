faa21cf637ab0807586221fa109c8f67c880d56b
max-s <max-s@DESKTOP-SGG2DJD>
2023-04-04 14:59:34 -0400

    trying it again

--- src/main/java/com/maxtrain/bootcamp/ers/expense/ExpenseRepository.java
+++ src/main/java/com/maxtrain/bootcamp/ers/expense/ExpenseRepository.java
@@ -1,9 +1,7 @@
 package com.maxtrain.bootcamp.ers.expense;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExpenseRepository extends CrudRepository <Expense, Integer> {
-    Optional<Expense> findByStatus(String status);
+    Iterable<Expense> findByStatus(String status);
 }
