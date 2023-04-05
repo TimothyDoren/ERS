d188f9703d2f0df763e9b6a63601155f4e29c955
Jacob Roberts <jacobroberts9797@hotmail.com>
2023-04-05 12:17:47 -0400

    added post, puts deletes, gets

--- src/main/java/com/maxtrain/bootcamp/ers/expenseline/ExpenselineController.java
+++ src/main/java/com/maxtrain/bootcamp/ers/expenseline/ExpenselineController.java
@@ -1,11 +1,17 @@
 package com.maxtrain.bootcamp.ers.expenseline;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.maxtrain.bootcamp.ers.expense.Expense;
 import com.maxtrain.bootcamp.ers.expense.ExpenseRepository;
 import com.maxtrain.bootcamp.ers.item.Item;
+import com.maxtrain.bootcamp.ers.item.ItemRepository;
 
 import jakarta.persistence.*;
 
@@ -16,6 +22,62 @@ public class ExpenselineController {
 	private ExpenseRepository expRepo;
 	@Autowired 
 	private ExpenselineRepository explRepo;
+	@Autowired
+	private ItemRepository itemRepo;
+	
 
+	@GetMapping
+	public ResponseEntity<Iterable<Expenseline>> getExpenselines() { 
+		Iterable<Expenseline> expenselines = explRepo.findAll();
+		return new ResponseEntity<Iterable<Expenseline>>(expenselines, HttpStatus.OK); 
+		
+	} 
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Expenseline> getExpenseline(@PathVariable int id) {
+		Optional<Expenseline> expenseline = explRepo.findById(id);
+		if(expenseline.isEmpty() ) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Expenseline>(expenseline.get(), HttpStatus.OK);
+	}
+
+	
+	@PostMapping
+	public ResponseEntity<Expenseline> postExpenseline (@RequestBody Expenseline expenseline) {
+		Expenseline newExpenseline = explRepo.save(expenseline);
+		Optional<Expense> expense = expRepo.findById(expenseline.getExpense().getId());
+		if(!expense.isEmpty() ) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		
+		return new ResponseEntity<Expenseline>(newExpenseline, HttpStatus.CREATED);
+		
+	}
+
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}")
+	public ResponseEntity putExpenseline (@PathVariable int id, @RequestBody Expenseline expenseline) {
+		if(expenseline.getId() != id) {
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+		
+		
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+		
+	}
+
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping("{id}")
+	public ResponseEntity deleteExpenseline(@PathVariable int id) {
+		Optional<Expenseline> expenseline = explRepo.findById(id);
+		if(expenseline.isEmpty()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		
+		explRepo.delete(expenseline.get());
+		
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 	
 }
